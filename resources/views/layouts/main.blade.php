0500cbf793fb4aaab478892bdac5cbfbcecffa65
Oscar Quintero <oscar24quintero@hotmail.com>
2021-07-02 18:23:16 -0500

    Creacion del layout principal

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>VerdurApp</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<navbar>
+		<ul>
+			<li><a href="/">VerdurApp</a></li>
+			<li>Productores</li>
+			<li>Acerca del proyecto</li>
+		</ul>
+		<ul>
+			<li><a href="/login">Log In</a></li>
+			<li><a href="/signup">Sign Up</a></li>
+		</ul>
+	</navbar>
+	<main>
+		<h1>VerdurApp</h1>
+		@yield('contenido')
+	</main>
+	
+</body>
+</html>

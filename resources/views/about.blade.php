9a95c82f82f3b07ae095841fe7c1be807cb3ddfc
Oscar Quintero <oscar24quintero@hotmail.com>
2021-07-02 20:57:20 -0500

    Descripcion en el archivo Acerca de

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.main')
 @section('contenido')
 	<h1>Acerca de VerdurApp</h1>
+
+	<p>
+		<strong>VerdurApp</strong> es una nueva forma de traer los distintos productos del campo a las personas que los buscan. Es una manera de poner en contacto al productor del campo con los distribuidores o clientes finales a través de un catalogo con los distintos porductos que ofrece cada uno de los productores. 	
+	</p>
+
+	<p>
+		Nace de la necesidad de conectar de manera sencilla a los diferentes productores del campo, ya sean grandes o pequeños con algún cliente que seguro los está buscando pero no sabe que están allí. Con VerdurApp ya no hay que buscar, puesto que desde que entras empiezas a encontrar.
+	</p>
+
+	<p>
+		VerdurApp by <i> Oscar Quintero</i>
+	</p>
 @endsection
